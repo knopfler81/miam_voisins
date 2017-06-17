cbd0864401e5456148ba7529e326fd44053a4a10
Nelly Duclos <knopfler42@yahoo.fr>
2017-06-18 00:46:51 +0200

    added images uploader for meals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617221335) do
+ActiveRecord::Schema.define(version: 20170617222348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170617221335) do
     t.integer  "user_id"
     t.string   "category"
     t.integer  "category_id"
+    t.string   "images"
     t.index ["user_id"], name: "index_meals_on_user_id", using: :btree
   end
 
@@ -78,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170617221335) do
     t.string   "token"
     t.datetime "token_expiry"
     t.string   "nickname"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
