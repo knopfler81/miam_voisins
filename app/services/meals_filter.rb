d70bf72d46a5eb357300d036e33466b498fbeb8c
Nelly Duclos <knopfler42@yahoo.fr>
2017-06-22 23:56:15 +0200

    filters locations

--- app/services/meals_filter.rb
+++ app/services/meals_filter.rb
@@ -2,21 +2,27 @@ class MealsFilter
 
   def initialize(params)
     @params = params
-    @meals = Meal.where('availability  >= ?', Date.today )
-    @meals = Meal.where('location LIKE?', @params[:query][:location]) if @params[:query][:location].present?
+    @meals = Meal.all #Meal.where('availability  >= ?', Date.today )
+    #@meals = Meal.where('location LIKE?', @params[:query][:location]) if @params[:query][:location].present?
   end
 
   def filter
+    filter_by_location
     filter_by_category
     @meals
   end
+
 
   private
 
   def filter_by_category
    if @params[:categories] != nil && @params[:categories].size != 0 && !@params[:categories].include?("all")
      @meals = @meals.joins(:category).where("categories.name IN (?)", @params[:categories])
+    #@meals = Meal.where('location LIKE?', @params[:query][:location]) if @params[:query][:location].present?
    end
   end
 
+  def filter_by_location
+    @meals = Meal.where('location LIKE?', @params[:query][:location]) if @params[:query][:location].present?
+  end
 end
