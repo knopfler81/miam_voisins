39e0dead1c0e21961a0b52bb59063a2566502524
Nelly Duclos <knopfler42@yahoo.fr>
2017-06-22 17:36:43 +0200

    changed action for the filtering

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,45 +1,45 @@
-var handler = Gmaps.build('Google');
+// var handler = Gmaps.build('Google');
 
-var markerJson = $('#map').data('markers');
-
-
-$(document).on('ready', function() {
-  if ($('#map').length > 0) {
-    drawMeAMap(markerJson);
-  }
-});
-
-function drawMeAMap(markerJson){
-  $('#map').html('');
-  handler.buildMap(
-    {
-      internal: { id: 'map' }
-
-    },
-    updateMarkers(markerJson),
-
-  );
-};
+// var markerJson = $('#map').data('markers');
 
 
-function updateMarkers(markerJson){
-  //alert("Bonjour");
+// $(document).on('ready', function() {
+//   if ($('#map').length > 0) {
+//     drawMeAMap(markerJson);
+//   }
+// });
 
-  handler.buildMap({ internal: { id: 'map' } }, function() {
-     markers = handler.addMarkers(markerJson);
-     handler.removeMarkers(marker);
-    });
-  handler.buildMap({ internal: { id: 'map' } }, function() {
-    markers = handler.addMarkers(markerJson);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    if (markers.length == 0) {
-      handler.getMap().setZoom(2);
-    } else if (markers.length == 1) {
-      handler.getMap().setZoom(14);
-    }
-  });
-};
+// function drawMeAMap(markerJson){
+//   $('#map').html('');
+//   handler.buildMap(
+//     {
+//       internal: { id: 'map' }
+
+//     },
+//     updateMarkers(markerJson),
+
+//   );
+// };
+
+
+// function updateMarkers(markerJson){
+//   //alert("Bonjour");
+
+//   handler.buildMap({ internal: { id: 'map' } }, function() {
+//      markers = handler.addMarkers(markerJson);
+//      handler.removeMarkers(marker);
+//     });
+//   handler.buildMap({ internal: { id: 'map' } }, function() {
+//     markers = handler.addMarkers(markerJson);
+//     handler.bounds.extendWith(markers);
+//     handler.fitMapToBounds();
+//     if (markers.length == 0) {
+//       handler.getMap().setZoom(2);
+//     } else if (markers.length == 1) {
+//       handler.getMap().setZoom(14);
+//     }
+//   });
+// };
 
 
 // function addMarkers(markerJson){
--- app/services/meals_filter.rb
+++ app/services/meals_filter.rb
@@ -2,12 +2,12 @@ class MealsFilter
 
   def initialize(params)
     @params = params
-    @meals = Meal.all
+    @meals = Meal.where('availability  >= ?', Date.today )
   end
 
   def filter
     filter_by_category
-    @meals = Meal.where('availability  >= ?', Date.today )
+    @meals
 
   end
 
